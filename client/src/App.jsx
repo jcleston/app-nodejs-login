fc101d65255c7a050ae1bdcb21deb2a4f411b8e2
janes silva <janes.cleston.silva@gmail.com>
2023-08-29 12:08:36 -0300

    feat: add routers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,33 @@ import Dashboard from './Components/Dashboard/Dashboard'
 import Login from './Components/Login/Login'
 import Register from './Components/Register/Register'
 
+//Import React router dom
+import{
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+// Create a router
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div><Login/></div>
+  },
+  {
+    path: '/register',
+    element: <div><Register/></div>
+  },
+  {
+    path: '/dashboard',
+    element: <div><Dashboard/></div>
+  }
+])
+
 function App() {
 
   return (
   <div>
-    <Dashboard/>
-    <Login/>
-    <Register/>
-    
+    <RouterProvider router={router}/>
   </div>
   )
 }
