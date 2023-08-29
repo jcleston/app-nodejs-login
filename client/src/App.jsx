92e6a850de5b0f971b5ceb8e93cd35a7f3c1df7a
janes silva <janes.cleston.silva@gmail.com>
2023-08-29 11:47:40 -0300

    feat: add modules + css and jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import Dashboard from './Components/Dashboard/Dashboard'
+import Login from './Components/Login/Login'
+import Register from './Components/Register/Register'
 
 function App() {
 
   return (
   <div>
-    App
+    <Dashboard/>
+    <Login/>
+    <Register/>
+    
   </div>
   )
 }
